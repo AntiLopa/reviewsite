3a404cb2916f81314a220a50b95d97e8a1d3ce93
AntiLopa <nirtheking@gmail.com>
2013-02-23 00:57:03 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reviewsite::Application.config.secret_token = 'a57b2837134a7e2f50f751eb962a06a850c242bea87d64523cb412cec09ded10f682568f2163c785b8afa889e3d07f0bf694581be04911d95d09be0b122a7fa8'
