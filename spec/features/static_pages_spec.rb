7bf8b7aa044a51916e669f393f5ee3696954aacc
AntiLopa <nirtheking@gmail.com>
2013-02-27 23:53:54 +0200

    fixed the rspec to pass

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,21 +2,24 @@ require 'spec_helper'
 
 #feature 'Static Pages' do
 
+
   feature 'Home page' do
+    subject {page.html}
 
     before { visit root_path}
 
-    scenario {expect(page).to have_selector('h1', text: 'Search Something')}
-    scenario {expect(page).to have_selector('title', text: full_title(''))}
+    scenario {should have_selector('h1', text: 'Search Something')}
+    scenario {should have_selector('title', text: full_title(''))}
 
   end
 
   feature 'Home page' do
+    subject {page.html}
 
     before { visit about_path}
 
-    scenario {expect(page).to have_selector('h1', text: 'About')}
-    scenario {expect(page).to have_selector('title', text: full_title('About'))}
+    scenario {should have_selector('h1', text: 'About')}
+    scenario {should have_selector('title', text: full_title('About'))}
 
   end
 
