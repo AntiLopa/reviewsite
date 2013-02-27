b96ddfb84e24fa5005394ed68ea5bb7ce15fa2cf
AntiLopa <nirtheking@gmail.com>
2013-02-28 00:23:15 +0200

    extend the static_pages_spec.rb

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,26 +1,53 @@
 require 'spec_helper'
 
-#feature 'Static Pages' do
+feature 'Home page' do
+  subject {page.html}
+
+  before { visit root_path}
+
+  scenario {should have_selector('h1', text: 'Search Something')}
+  scenario {should have_selector('title', text: full_title(''))}
 
 
-  feature 'Home page' do
-    subject {page.html}
+end
 
-    before { visit root_path}
+feature 'About page' do
+  subject {page.html}
 
-    scenario {should have_selector('h1', text: 'Search Something')}
-    scenario {should have_selector('title', text: full_title(''))}
+  before { visit about_path}
 
+  scenario {should have_selector('h1', text: 'About')}
+  scenario {should have_selector('title', text: full_title('About'))}
+
+end
+
+feature 'Check links' do
+  subject { page.html }
+
+  before { visit root_path }
+
+  scenario do
+    click_link 'About'
+    should have_selector('title', text: full_title('About'))
   end
 
-  feature 'Home page' do
-    subject {page.html}
-
-    before { visit about_path}
-
-    scenario {should have_selector('h1', text: 'About')}
-    scenario {should have_selector('title', text: full_title('About'))}
-
+  scenario do
+    click_link 'snfer'
+    should have_selector('title', text: full_title(''))
   end
 
-#end
+  #scenario do
+  #  click_link 'Cameras'
+  #  should have_selector('title', text: full_title('Cameras'))
+  #end
+  #
+  #scenario do
+  #  click_link 'TVs'
+  #  should have_selector('title', text: full_title('TVs'))
+  #end
+  #
+  #scenario do
+  #  click_link 'Laptops'
+  #  should have_selector('title', text: full_title('Laptops'))
+  #end
+end
